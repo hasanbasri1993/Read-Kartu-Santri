46b4191505a621e92daa9da02f62f55fcc583eb6
Hasan Basri <hasanbasri1493@gmail.com>
2021-10-14 21:16:07 +0700

    user NicoHood HID lib

--- SerialPassthrough/SerialPassthrough.ino
+++ SerialPassthrough/SerialPassthrough.ino
@@ -1,39 +1,15 @@
-/*
-  SerialPassthrough sketch
-
-  Some boards, like the Arduino 101, the MKR1000, Zero, or the Micro, have one
-  hardware serial port attached to Digital pins 0-1, and a separate USB serial
-  port attached to the IDE Serial Monitor. This means that the "serial
-  passthrough" which is possible with the Arduino UNO (commonly used to interact
-  with devices/shields that require configuration via serial AT commands) will
-  not work by default.
-
-  This sketch allows you to emulate the serial passthrough behaviour. Any text
-  you type in the IDE Serial monitor will be written out to the serial port on
-  Digital pins 0 and 1, and vice-versa.
-
-  On the 101, MKR1000, Zero, and Micro, "Serial" refers to the USB Serial port
-  attached to the Serial Monitor, and "Serial1" refers to the hardware serial
-  port attached to pins 0 and 1. This sketch will emulate Serial passthrough
-  using those two Serial ports on the boards mentioned above, but you can change
-  these names to connect any two serial ports on a board that has multiple ports.
-
-  created 23 May 2016
-  by Erik Nyquist
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/SerialPassthrough
-*/
-
-#include "Keyboard.h"
+#include "HID-Project.h"
 
 void setup() {
   Serial1.begin(115200);
+  Keyboard.begin();
 }
 
 void loop() {
-  if (Serial1.available()) {  // If anything comes in Serial1 (pins 0 & 1)
-    Keyboard.println(Serial1.readString());
-    Serial.write(Serial1.read());  // read it and send it out Serial (USB)
+  if (Serial1.available()) {  
+    Keyboard.print(Serial1.readString());
+    delay(50);
+    Keyboard.write(KEY_ENTER);
+    delay(300);
   }
-  delay(200);
 }
