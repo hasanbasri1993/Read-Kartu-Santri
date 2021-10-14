44f6755f9db709e1776a2a8ad71bc22dfb84c84c
Hasan Basri <hasanbasri1493@gmail.com>
2021-10-14 20:27:17 +0700

    add delay arduino leonardo

--- ReadTagExtended/ReadTagExtended.ino
+++ ReadTagExtended/ReadTagExtended.ino
@@ -9,47 +9,37 @@ PN532_I2C pn532_i2c(Wire);
 NfcAdapter nfc = NfcAdapter(pn532_i2c);
 #define OUT_LED 13
 #define OUT_BUZZER 12
-void setup(void)
-{
+void setup() {
   pinMode(OUT_LED, OUTPUT);
   pinMode(OUT_BUZZER, OUTPUT);
   digitalWrite(OUT_LED, HIGH);
   digitalWrite(OUT_BUZZER, LOW);
-  Serial.begin(115200); //This pipes to the serial monitor
+  Serial.begin(115200);  //This pipes to the serial monitor
   Serial1.begin(115200);
   Serial.println("Initialize Serial Monitor");
   Serial.println("NDEF Reader");
   nfc.begin();
 }
 
-void loop(void)
-{
+void loop() {
   Serial.println("Scan a NFC tag");
-  if (nfc.tagPresent())
-  {
+  if (nfc.tagPresent()) {
     NfcTag tag = nfc.read();
-    Serial.println(tag.getTagType());
-    Serial.print("UID: ");
-    Serial.println(tag.getUidString());
-
-    if (tag.hasNdefMessage()) // every tag won't have a message
+    //    Serial.println(tag.getTagType());
+    //    Serial.print("UID: ");
+    //    Serial.println(tag.getUidString());
+    if (tag.hasNdefMessage())  // every tag won't have a message
     {
-
       NdefMessage message = tag.getNdefMessage();
-      Serial.print("\nThis NFC Tag contains an NDEF Message with ");
-      Serial.print(message.getRecordCount());
-      Serial.print(" NDEF Record");
-      if (message.getRecordCount() != 1)
-      {
-        Serial.print("s");
-      }
-      Serial.println(".");
-      Serial.println("\nNDEF Record ");
-      NdefRecord record = message[0]; // alternate syntax
-      
+      //      Serial.print("\nThis NFC Tag contains an NDEF Message with ");
+      //      Serial.print(message.getRecordCount());
+      //      Serial.print(" NDEF Record");
+
+      //      Serial.println(".");
+      //      Serial.println("\nNDEF Record ");
+      NdefRecord record = message[0];  // alternate syntax
       //Serial.print("  TNF: ");Serial.println(record.getTnf());
       //Serial.print("  Type: ");  Serial.println(record.getType()); // will be "" for TNF_EMPTY
-      
       int payloadLength = record.getPayloadLength();
       byte payload[payloadLength];
       record.getPayload(payload);
@@ -66,34 +56,37 @@ void loop(void)
       //Serial.print("Decoded string is:\t");Serial.println(decodedString);
       StaticJsonDocument<512> doc;
       DeserializationError error = deserializeJson(doc, decodedString);
-      if (error)
-      {
+      if (error) {
         Serial.print(F("deserializeJson() failed: "));
         Serial.println(error.f_str());
         return;
+      }else{
+        const char *id = doc["id"];        // "6447"
+        const char *induk = doc["induk"];  // "206085"
+        Serial.println(induk);
+        Serial.println(id);
+        if (id != "") {
+          Serial1.write(induk);
+          notif();      
+        }
       }
-      const char *id = doc["id"];       // "6447"
-      const char *induk = doc["induk"]; // "206085"
-      Serial.println(induk);
-      Serial.println(id);
-      if (id != "")
-      {
-        Serial1.write(induk);
-        digitalWrite(OUT_BUZZER, HIGH);
-        digitalWrite(OUT_LED, LOW);
-        delay(500);
-        digitalWrite(OUT_BUZZER, LOW);
-        digitalWrite(OUT_LED, HIGH);
-      }
-
+      
       // id is probably blank and will return ""
       String uid = record.getId();
-      if (uid != "")
-      {
+      if (uid != "") {
         Serial.print("  ID: ");
         Serial.println(uid);
       }
     }
   }
-  delay(500);
+  delay(300);
+}
+
+void notif()
+{
+    digitalWrite(OUT_BUZZER, HIGH);
+    digitalWrite(OUT_LED, LOW);
+    delay(300);
+    digitalWrite(OUT_BUZZER, LOW);
+    digitalWrite(OUT_LED, HIGH);
 }
--- SerialPassthrough/SerialPassthrough.ino
+++ SerialPassthrough/SerialPassthrough.ino
@@ -27,16 +27,13 @@
 #include "Keyboard.h"
 
 void setup() {
-  Serial.begin(115200);
   Serial1.begin(115200);
 }
-void loop() {
-  if (Serial.available()) {      // If anything comes in Serial (USB),
-    Serial1.write(Serial.read());   // read it and send it out Serial1 (pins 0 & 1)
-  }
 
-  if (Serial1.available()) {     // If anything comes in Serial1 (pins 0 & 1)
+void loop() {
+  if (Serial1.available()) {  // If anything comes in Serial1 (pins 0 & 1)
     Keyboard.println(Serial1.readString());
-    Serial.write(Serial1.read());   // read it and send it out Serial (USB)
+    Serial.write(Serial1.read());  // read it and send it out Serial (USB)
   }
+  delay(200);
 }
